6aa4f77a2ee9238c2fcbe580a1fab41a70aadd8e
Darshil12345 <91784452+Darshil12345@users.noreply.github.com>
2026-06-18 02:46:40 +0530

    Change template directory name to 'Templates'

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
